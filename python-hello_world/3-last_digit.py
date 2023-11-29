3ba301f97eb93deb2cba70692c0f96c9f997931b
Wuabu <wuabueric98@gmail>
2023-11-29 08:00:21 +0000

    And

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 print ("Last number of {} is {}".format(number, abs(number) % 10), end= "")
 
 if abs(number) % 10 > 5:
-    print (" is greater than 5")
+    print (" and is greater than 5")
     
 elif abs(number) % 10 == 0:
     print (" and is 0")
