970bd6af746bb77d9eb36a4ffddc81bbfc40de55
Wuabu <wuabueric98@gmail>
2023-11-29 15:54:05 +0000

    000sum

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,12 +1,6 @@
-#add = __import__('0-sum').add
-
-#print(add(1, 2))
-#print(add(98, 0))
-#print(add(100, -2))
-a=input
-b=input
 def add(a, b):
     return a + b
 
-result = add(a, b)
-print(result)  # Output: 8
+print(add(1, 2))     
+print(add(98, 0))    
+print(add(100, -2))
