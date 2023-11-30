6d097faae30622cd7241e9bdd99cf7cbfe72d1c2
Wuabu <wuabueric98@gmail>
2023-11-30 15:17:36 +0000

    Power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,3 +1,13 @@
 def pow(a, b):
-    return a ^ b
-print: (pow(5, 2))
+    # Base case: any number to the power of 0 is 1
+    if b == 0:
+        return 1
+    
+    # Initialize result to the base value
+    result = a
+    
+    # Multiply 'result' by 'a' 'b-1' times
+    for _ in range(1, b):
+        result *= a
+    
+    return result
