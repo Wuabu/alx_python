ea1f05d46af0cad89a1486a7e1f11b1ae5dce4db
Wuabu <wuabueric98@gmail>
2023-11-30 15:54:34 +0000

    change

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -23,7 +23,7 @@ def validate_password(password):
     
     return has_uppercase and has_lowercase and has_digit
 
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+#print(validate_password("Password123"))
+#print(validate_password("abc123"))
+#print(validate_password("Password 123"))
+#print(validate_password("password123"))
