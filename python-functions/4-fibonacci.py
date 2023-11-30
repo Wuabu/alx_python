444bae1a4fd12fff499a00cce05c68e6a74ec334
Wuabu <wuabueric98@gmail>
2023-11-30 15:42:21 +0000

    1

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,16 +1,16 @@
 def fibonacci_sequence(n):
-    # Check if n is not a positive integer
+    
     if not isinstance(n, int) or n <= 0:
         return []
 
-    # Initialize the Fibonacci sequence with the first two numbers
-    sequence = [0, 1]
+    
+    sequence = [0, ]
 
-    # Generate the Fibonacci sequence up to the nth number
+    
     while len(sequence) < n:
         next_number = sequence[-1] + sequence[-2]
         sequence.append(next_number)
 
     return sequence
 
-# Test cases
+
