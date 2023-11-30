804f6101885f3f9f2a656dd8ab25acb9e0070352
Wuabu <wuabueric98@gmail>
2023-11-30 15:30:04 +0000

    temperature

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,14 +1,14 @@
 def pow(a, b):
-    # Base case: any number to the power of 0 is 1
+   
     if b == 0:
         return 1
     
     if b < 0:
         return 1 / pow(a, -b) 
-    # Initialize result to the base value
+   
     result = a
     
-    # Multiply 'result' by 'a' 'b-1' times
+    
     for _ in range(1, b):
         result *= a
     
