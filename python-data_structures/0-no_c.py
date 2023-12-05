c29ab76d5b14901bc9f195363611ed7a10004df8
Wuabu <wuabueric98@gmail>
2023-12-05 12:32:42 +0000

    New no C

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,3 +1,11 @@
-i = "Chicago"
-a = ''.join(c for c in i if c not in {'C', 'c'})
-print(a)
+# i = input
+# a = ''.join(c for c in i if c not in {'C', 'c'})
+# print(a)
+
+def no_c(my_string):
+    return ''.join(char for char in my_string if char.lower() != 'c')
+
+# if __name__ == "__main__":
+# print(no_c("Holberton School"))
+# print(no_c("Chicago"))
+# print(no_c("C is fun!"))
