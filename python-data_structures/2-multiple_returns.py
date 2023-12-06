a77aa74349d5fe969ce7aab8c67d2ba6b9b84cba
Wuabu <wuabueric98@gmail>
2023-12-06 11:52:37 +0000

    2mul

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -5,11 +5,11 @@ def multiple_returns(sentence):
     
     
     return len(sentence), sentence[0]
-    
-# Example usage:
-result = multiple_returns("At Holberton school, I learnt C!")
+# if __name__ == "__main__":
+# # Example usage:
+# result = multiple_returns("At Holberton school, I learnt C!")
 
-print("Length: {} - First character: {}".format(result[0], result[1]) )
+# print("Length: {} - First character: {}".format(result[0], result[1]) )
 
-empty_result = multiple_returns("")
-print(empty_result)  
+# empty_result = multiple_returns("")
+# print(empty_result)  
