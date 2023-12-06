56c2cf5ffa11ddeb9420bb68ba2276c62beed150
Wuabu <wuabueric98@gmail>
2023-12-06 11:53:35 +0000

    Good

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,15 +1,7 @@
 def multiple_returns(sentence):
-    # Check if the sentence is empty
+    
     if not sentence:
         return None
     
     
     return len(sentence), sentence[0]
-# if __name__ == "__main__":
-# # Example usage:
-# result = multiple_returns("At Holberton school, I learnt C!")
-
-# print("Length: {} - First character: {}".format(result[0], result[1]) )
-
-# empty_result = multiple_returns("")
-# print(empty_result)  
