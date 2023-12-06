194841f743b37dfe5b9fb2096f6140151c02f569
Wuabu <wuabueric98@gmail>
2023-12-06 08:13:41 +0000

    Matrix

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,11 +1,3 @@
-# i = input
-# a = ''.join(c for c in i if c not in {'C', 'c'})
-# print(a)
-
 def no_c(my_string):
-    return ''.join(char for char in my_string if char.lower() != 'c')
+    return ''.join(c for c in my_string if c.lower() != 'c')
 
-# if __name__ == "__main__":
-# print(no_c("Holberton School"))
-# print(no_c("Chicago"))
-# print(no_c("C is fun!"))
