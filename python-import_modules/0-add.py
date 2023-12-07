8c187b74bb10c6415bec3407e1bc6b40fb73d353
Wuabu <wuabueric98@gmail.com>
2023-12-07 22:46:05 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,43 +1,7 @@
-# def add(a, b):
-    
-    
-#     a= int("a")
-#     b= int("b")
-#     return a + b
-# a= input
-# b= input
-# add = a + b
+from add_0 import add
 
-# print   ("{} + {} = {}".format(a, b, add))
-
-
-# def add(a, b):
-#     return a + b
-
-# if __name__ == "__main__":
-#     # Your code here for direct execution
-#     a = 5
-#     b = 9
-#     result = add(a, b)
-# print(f"{a} + {b} = {result}")
-
-
-# def add(a, b):
-#     return a + b
-
-# if __name__ == "__main__":
-#     # Your code here for direct execution
-#     a = 5
-#     b = 9
-#     # add_0 = add  # Rename add to add_0
-#     result = add_0 = add(a, b)
-#     print(f"{a} + {b} = {result}")
-
-
-# if __name__ == "__main__":
-#     from add_0 import add
-#     a = 1
-#     b = 2
-#     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
-
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
