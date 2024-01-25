39b8f39bf97d24ccbee61a58e40417f5787df9ba
Wuabu <wuabueric98@gmail>
2024-01-25 16:23:02 +0000

    c+text

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,51 @@
+"""
+Simple Flask Web Application
+
+This module defines a simple Flask web application with one route.
+
+Usage:
+- Run the script to start the web application.
+- Access the home route at http://0.0.0.0:5000/ in a web browser or using curl.
+
+Routes:
+- `/`: Displays a greeting message "Hello HBNB!" in an h1 HTML tag.
+
+Note:
+- The application runs on 0.0.0.0, port 5000.
+- The route definition uses `strict_slashes=False` to allow access with or without a trailing slash.
+"""
+
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    Home Route
+
+    Displays a greeting message "Hello HBNB!" in an h1 HTML tag.
+
+    Returns:
+    str: HTML-formatted greeting message.
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays the text HBHN in an h1 HTML tag.
+
+    Returns:
+        str: HTML formatted message
+    """
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text():
+    sentence =text.replace ('_', ' ')
+    return "C {sentence}"
+
+if __name__ == '__main__':
+    # Run the Flask application on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
