803eb77f34052cd2dd7c638788d21c6268ecf272
Wuabu <wuabueric98@gmail>
2024-01-25 16:03:08 +0000

    documented

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,8 +1,37 @@
-from flask import Flask
-app = Flask (__name__)
+"""
+Simple Flask Web Application
 
-@app.route ("/", strict_slashes=False)
+This module defines a simple Flask web application with one route.
+
+Usage:
+- Run the script to start the web application.
+- Access the home route at http://0.0.0.0:5000/ in a web browser or using curl.
+
+Routes:
+- `/`: Displays a greeting message "Hello HBNB!" in an h1 HTML tag.
+
+Note:
+- The application runs on 0.0.0.0, port 5000.
+- The route definition uses `strict_slashes=False` to allow access with or without a trailing slash.
+"""
+
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    return "<h1> Hello HBNB </h1>"
+    """
+    Home Route
+
+    Displays a greeting message "Hello HBNB!" in an h1 HTML tag.
+
+    Returns:
+    str: HTML-formatted greeting message.
+    """
+    return "<h1>Hello HBNB</h1>"
+
 if __name__ == '__main__':
+    # Run the Flask application on 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000)
