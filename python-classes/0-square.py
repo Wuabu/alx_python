5f0d9051a2b89d19285338e8e13834fd8bb7fe22
Wuabu <wuabueric98@gmail>
2024-02-27 06:13:10 +0000

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,25 @@
 class Square:
-    def __init__(self, size):
-        self.__size = size
+    """
+    This class represents a square.
 
-# Example usage:
-square_instance = Square(5)
-# Accessing the size attribute directly will result in an AttributeError
-# print(square_instance.__size)
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
